b516cba1e1b251ae5ce8df4ae07bf643b1dd9e5a
CoolPixels <coolp_xels@proton.me>
2025-01-19 16:59:50 +0000

    Testing Sun Jan 19 2025 16:59:49 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex01/student/main.cpp
+++ chapter2/ex01/student/main.cpp
@@ -4,5 +4,11 @@ using namespace std;
 
 int main() {
     // Write your main here
+    std::cout << "**********************************" << std::endl << std::endl;
+    std::cout << "*    Programming Assignment 1    *" << std::endl << std::endl;
+    std::cout << "*     Computer Programming I     *" << std::endl << std::endl;
+    std::cout << "*       Author: Isaac Wilson     *" << std::endl << std::endl;
+    std::cout << "*   Due Date: Thursday, Jan. 24  *" << std::endl << std::endl;
+    std::cout << "**********************************" << std::endl;
     return 0;
 }
