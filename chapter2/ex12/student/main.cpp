e28254a138d8540335c6ec01506ef8e8a986692a
CoolPixels <coolp_xels@proton.me>
2025-01-19 18:11:28 +0000

    Testing Sun Jan 19 2025 18:11:28 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex12/student/main.cpp
+++ chapter2/ex12/student/main.cpp
@@ -12,9 +12,9 @@ int main()
 
     const int hours = int(timeSeconds / 3600);
     const int minutes = int((timeSeconds % 3600) / 60);
-    const int seconds = int((timeSeconds % 3600) & 60);
+    const int seconds = int((timeSeconds % 3600) % 60);
 
-    cout << hours << ":" << minutes << ":" << seconds;
+    cout << hours << ":" << minutes << ":" << seconds << endl;
 
     return 0;
 }
