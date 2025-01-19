88885d5341765cc10d994f59fbb9f9a4fb0936aa
CoolPixels <coolp_xels@proton.me>
2025-01-19 19:59:53 +0000

    Testing Sun Jan 19 2025 19:59:53 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex23/student/main.cpp
+++ chapter2/ex23/student/main.cpp
@@ -2,7 +2,35 @@
 
 using namespace std;
 
-int main() {
-    // Write your main here
+int main()
+{
+    // variables
+    double buyingPrice;
+    double sellingPrice;
+    int sharesSold;
+
+    // get input
+    cout << "Buying price: $";
+    cin >> buyingPrice;
+    cout << endl;
+
+    cout << "Selling price: $";
+    cin >> sellingPrice;
+    cout << endl;
+
+    cout << "Shares sold: ";
+    cin >> sharesSold;
+    cout << endl;
+
+    // run calculations
+    const double investment = buyingPrice * sharesSold;
+    const double income = sellingPrice * sharesSold;
+    cout << "Invested: $" << investment << endl;
+    cout << "Received: $" << income << endl;
+    const double serviceCharge = (income + investment) * 0.015;
+    cout << "Service charge: $" << serviceCharge << endl;
+    cout << "Delta: $" << income - investment - serviceCharge << endl;
+
+    // return
     return 0;
 }
