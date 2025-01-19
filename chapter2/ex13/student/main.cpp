acadb6e603ad8cc38dd502893fdb623d6e83429d
CoolPixels <coolp_xels@proton.me>
2025-01-19 19:45:27 +0000

    Testing Sun Jan 19 2025 19:45:27 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex13/student/main.cpp
+++ chapter2/ex13/student/main.cpp
@@ -2,7 +2,34 @@
 
 using namespace std;
 
-int main() {
-    // Write your main here
+int main()
+{
+    // variables
+    double originalPrice;
+    double markupPercent;
+    double taxRatePercent;
+
+    // get data
+    cout << "Enter the original price of the item: ";
+    cin >> originalPrice;
+    cout << endl;
+
+    cout << "Enter the markup percentage: ";
+    cin >> markupPercent;
+    cout << endl;
+
+    cout << "Enter the sales tax percentage: ";
+    cin >> taxRatePercent;
+
+    // run calculations
+    cout << endl;
+    cout << "Original price: $" << originalPrice << endl;
+    cout << "Markup: " << markupPercent << "%" << endl;
+    cout << "Selling price: $" << originalPrice * (1 + markupPercent / 100) << endl;
+    cout << "Sales tax rate: " << taxRatePercent << "%" << endl;
+    cout << "Sales tax: $" << originalPrice * (1 + markupPercent / 100) * taxRatePercent / 100 << endl;
+    cout << "Final price: $" << originalPrice * (1 + markupPercent / 100) * taxRatePercent / 100 + originalPrice * (1 + markupPercent / 100) << endl;
+
+    // return
     return 0;
 }
