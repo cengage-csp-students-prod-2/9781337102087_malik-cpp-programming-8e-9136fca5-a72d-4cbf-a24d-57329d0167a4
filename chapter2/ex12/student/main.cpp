dc0a5a2d2e7c8689dc86a8a5f6dcbb96c6cf282c
CoolPixels <coolp_xels@proton.me>
2025-01-19 18:09:43 +0000

    Testing Sun Jan 19 2025 18:09:43 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex12/student/main.cpp
+++ chapter2/ex12/student/main.cpp
@@ -2,7 +2,19 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     // Write your main here
+    int timeSeconds;
+
+    cout << "Input the duration in seconds:" << endl;
+    cin >> timeSeconds;
+
+    const int hours = int(timeSeconds / 3600);
+    const int minutes = int((timeSeconds % 3600) / 60);
+    const int seconds = int((timeSeconds % 3600) & 60);
+
+    cout << hours << ":" << minutes << ":" << seconds;
+
     return 0;
 }
