849e052919b9b0f9af970ee8e4760ac9272477d1
CoolPixels <coolp_xels@proton.me>
2025-01-19 17:55:32 +0000

    Testing Sun Jan 19 2025 17:55:32 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex10/student/main.cpp
+++ chapter2/ex10/student/main.cpp
@@ -2,7 +2,30 @@
 
 using namespace std;
 
-int main() {
-    // Write your main here
+int main()
+{
+    double num1;
+    double num2;
+    double num3;
+    double num4;
+    double num5;
+
+    cout << "#1:" << endl;
+    cin >> num1;
+
+    cout << "#2:" << endl;
+    cin >> num2;
+    
+    cout << "#3:" << endl;
+    cin >> num3;
+    
+    cout << "#4:" << endl;
+    cin >> num4;
+    
+    cout << "#5:" << endl;
+    cin >> num5;
+
+    cout << "Average: " << (num1 + num2 + num3 + num4 + num5) / 5 << endl; 
+
     return 0;
 }
