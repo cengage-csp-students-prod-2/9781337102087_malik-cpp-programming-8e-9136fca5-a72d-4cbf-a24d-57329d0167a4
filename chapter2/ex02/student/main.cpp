dc70cce0cca658d6c04988f56d36e56c9bfef080
CoolPixels <coolp_xels@proton.me>
2025-01-19 16:53:32 +0000

    Testing Sun Jan 19 2025 16:53:32 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex02/student/main.cpp
+++ chapter2/ex02/student/main.cpp
@@ -5,4 +5,9 @@ using namespace std;
 int main() {
     // Write your main here
     std::cout << "CCCCCCCCC       ++              ++" << std::endl;
+    std::cout << "CC              ++              ++" << std::endl;
+    std::cout << "CC        ++++++++++++++  +++++++++++++++" << std::endl;
+    std::cout << "CC        ++++++++++++++  +++++++++++++++" << std::endl;
+    std::cout << "CC              ++              ++" << std::endl;
+    std::cout << "CCCCCCCCC       ++              ++" << std::endl;
 }
