e22d00ca13573a2d80c1c60316ad16b33abd02a5
CoolPixels <coolp_xels@proton.me>
2025-01-25 01:38:37 +0000

    Testing Sat Jan 25 2025 01:38:37 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex26/student/main.cpp
+++ chapter2/ex26/student/main.cpp
@@ -2,7 +2,74 @@
 
 using namespace std;
 
-int main() {
+double main()
+{
+    // variables
+    double doorWidth;
+    double doorHeight;
+
+    double window1Width;
+    double window1Height;
+
+    double window2Width;
+    double window2Height;
+
+    double bookshelfWidth;
+    double bookshelfHeight;
+
+    double roomLength;
+    double roomWidth;
+    double roomHeight;
+
+    // get input
+    cout << "Door width: ";
+    cin >> doorWidth;
+
+    cout << "Door height: ";
+    cin >> doorHeight;
+
+    cout << "Window 1 width: ";
+    cin >> window1Width;
+
+    cout << "Window 1 height: ";
+    cin >> window1Height;
+
+    cout << "Window 2 width: ";
+    cin >> window2Width;
+
+    cout << "Window 2 height: ";
+    cin >> window2Height;
+
+    cout << "Bookshelf width: ";
+    cin >> window2Width;
+
+    cout << "Bookshelf height: ";
+    cin >> bookshelfHeight;
+
+    cout << "Bookshelf width: ";
+    cin >> bookshelfWidth;
+
+    cout << "Room length: ";
+    cin >> roomLength;
+
+    cout << "Room width: ";
+    cin >> roomWidth;
+
+    cout << "Room height: ";
+    cin >> roomHeight;
+
+    // run calculations
+    const double basePadoubleArea = roomLength * roomHeight * 2 + roomWidth * roomHeight * 2;
+    const double doorArea = doorWidth * doorHeight;
+    const double window1Area = window1Width * window1Height;
+    const double window2Area = window2Width * window2Height;
+    const double bookshelfArea = bookshelfWidth * bookshelfHeight;
+    const double negativeArea = doorArea + window1Area + window2Area + bookshelfArea;
+    const double padoubleingArea = basePadoubleArea - negativeArea;
+    const double padoubleGallons = padoubleingArea / 120;
+
+    cout << "You need" << padoubleGallons << "gallons of paint";
+
     // Write your main here
     return 0;
 }
