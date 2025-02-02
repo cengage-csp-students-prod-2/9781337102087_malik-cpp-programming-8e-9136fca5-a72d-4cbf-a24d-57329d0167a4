f473810f5e33d5544951f561c8e8991c58d2f3b9
CoolPixels <coolp_xels@proton.me>
2025-02-02 18:30:06 +0000

    Testing Sun Feb 02 2025 18:30:06 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex04/student/main.cpp
+++ chapter3/ex04/student/main.cpp
@@ -1,36 +1,35 @@
-//Logic errors.
 #include <iostream>
 #include <iomanip>
 
 using namespace std;
 
 int main()
-{  
-    double cost;
-    double area;
-    double bagSize;
-    
-    cout << fixed << showpoint << setprecision(2);
-    
-    cout << "Enter the amount of fertilizer, in pounds, "
-         << "in one bag: ";
-    cin >> bagSize;
-    cout << endl;
-    
-    cout << "Enter the cost of the " << bagSize
-         << " pound fertilizer bag: ";
-    cin >> cost;
-    cout << endl;
-    
-    cout << "Enter the area, in square feet, that can be "
-         << "fertilized by one bag: ";
-    cin >> area;
-    cout << endl;
-    
-    cout << "The cost of the fertilizer per pound is: $"
-         << bagSize / cost << endl;
-    
-    cout << "The cost of fertilizing per square foot is: $"
-         << area / cost << endl;
-    return 0;
+{
+     double cost;
+     double area;
+     double bagSize;
+
+     cout << fixed << showpoint << setprecision(2);
+
+     cout << "Enter the amount of fertilizer, in pounds, "
+          << "in one bag: ";
+     cin >> bagSize;
+     cout << endl;
+
+     cout << "Enter the cost of the " << bagSize
+          << " pound fertilizer bag: ";
+     cin >> cost;
+     cout << endl;
+
+     cout << "Enter the area, in square feet, that can be "
+          << "fertilized by one bag: ";
+     cin >> area;
+     cout << endl;
+
+     cout << "The cost of the fertilizer per pound is: $" << fixed << setprecision(2)
+          << cost / bagSize << endl;
+
+     cout << "The cost of fertilizing per square foot is: $" << fixed << setprecision(2)
+          << cost / area << endl;
+     return 0;
 }
