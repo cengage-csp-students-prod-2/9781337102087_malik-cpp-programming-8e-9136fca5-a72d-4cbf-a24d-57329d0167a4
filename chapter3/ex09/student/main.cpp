47b4db7d5f6923a2630e719d17d8c46630121104
CoolPixels <coolp_xels@proton.me>
2025-02-03 00:47:20 +0000

    Testing Mon Feb 03 2025 00:47:20 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex09/student/main.cpp
+++ chapter3/ex09/student/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <iomanip>
+#include <cmath>
 
 using namespace std;
 
-int main() {
+int main()
+{
+    // constants
     const double pi = 3.141593;
+
+    // get input
+    double cylinderRadius;
+    double cylinderHeight;
+
+    cout << "Cylinder radius: ";
+    cin >> cylinderRadius;
+    cout << "Cylinder height: ";
+    cin >> cylinderHeight;
+
+    // calculate
+    const double cylinderVolume = pi * pow(cylinderRadius, 2) * cylinderHeight;
+    const double cubeSideLength = cbrt(cylinderVolume);
+
+    cout << "Cube side length: " << cubeSideLength << " units";
 
     // Write your main here
     return 0;
