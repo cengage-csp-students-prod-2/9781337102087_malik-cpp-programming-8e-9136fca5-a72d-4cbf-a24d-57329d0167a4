9f8b5b4908cff31eed43d760d404315df46d8f6d
CoolPixels <coolp_xels@proton.me>
2025-02-03 02:50:11 +0000

    Testing Mon Feb 03 2025 02:50:11 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex11/student/main.cpp
+++ chapter3/ex11/student/main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main() {
-    // Write your main here
+    // get input
+    double l, w, h;
+    double volumeReduction;
+
+    cout << "Length, width, and height: ";
+    cin >> l >> w >> h;
+    
+    cout << "Volume reduction percentage: ";
+    cin >> volumeReduction;
+
+    // run logic
+    const double oldVolume = l * w * h;
+    const double newVolume = oldVolume * (1 - volumeReduction / 100);
+
+    const double scaleFactor = sqrt(newVolume / oldVolume);
+
+    const double newL = l * scaleFactor;
+    const double newW = w * scaleFactor;
+
+    // output
+    cout << newL << " x " << newW << " x " << h;
+
     return 0;
 }
