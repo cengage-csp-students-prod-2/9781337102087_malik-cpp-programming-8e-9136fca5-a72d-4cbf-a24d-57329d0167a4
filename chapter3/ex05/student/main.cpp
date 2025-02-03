5e73b012aa24af5b0e28b1c0b60f0615808b1c68
CoolPixels <coolp_xels@proton.me>
2025-02-03 00:31:44 +0000

    Testing Mon Feb 03 2025 00:31:43 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex05/student/main.cpp
+++ chapter3/ex05/student/main.cpp
@@ -1,8 +1,50 @@
 #include <iostream>
+#include <fstream>
+#include <iomanip>
 
 using namespace std;
 
-int main() {
-    // Write your main here
+int main()
+{
+    // ask for file
+    string fileName;
+    cout << "Input file name: ";
+    cin >> fileName;
+
+    // open files
+    ifstream inputFile;
+    inputFile.open(fileName);
+
+    ofstream outputFile;
+    outputFile.open("Ch3_Ex5Output.dat");
+
+    // parse data
+    string fn1, fn2, fn3;
+    string ln1, ln2, ln3;
+    double s1, s2, s3;
+    double i1, i2, i3;
+
+    inputFile >> fn1;
+    inputFile >> ln1;
+    inputFile >> s1;
+    inputFile >> i1;
+    inputFile >> fn2;
+    inputFile >> ln2;
+    inputFile >> s2;
+    inputFile >> i2;
+    inputFile >> fn3;
+    inputFile >> ln3;
+    inputFile >> s3;
+    inputFile >> i3;
+
+    // write to output file
+    outputFile << fn1 << " " << ln1 << " " << fixed << setprecision(2) << s1 * (double(1) + (i1 / double(100))) << '\n';
+    outputFile << fn2 << " " << ln2 << " " << fixed << setprecision(2) << s2 * (double(1) + (i2 / double(100))) << '\n';
+    outputFile << fn3 << " " << ln3 << " " << fixed << setprecision(2) << s3 * (double(1) + (i3 / double(100)));
+
+    // close files
+    inputFile.close();
+    outputFile.close();
+
     return 0;
 }
--- chapter3/ex09/student/main.cpp
+++ chapter3/ex09/student/main.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 int main() {
+    const double pi = 3.141593;
+
     // Write your main here
     return 0;
 }
