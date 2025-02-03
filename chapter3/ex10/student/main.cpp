62b291341b74888e54349506a80e9c969e0131b7
CoolPixels <coolp_xels@proton.me>
2025-02-03 01:13:55 +0000

    Testing Mon Feb 03 2025 01:13:55 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex10/student/main.cpp
+++ chapter3/ex10/student/main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-int main() {
-    // Write your main here
+int main()
+{
+    // constants
+    const double pi = 3.14159;
+
+    // get input
+    double yardLength;
+    double treeRadius;
+    double treeSpace;
+
+    cout << "Yard side length: ";
+    cin >> yardLength;
+    cout << "Tree radius: ";
+    cin >> treeRadius;
+    cout << "Space between each tree";
+    cin >> treeSpace;
+
+    // run calculations
+    const int treeCount = yardLength / treeSpace;
+    const int treeArea = pi * pow(treeRadius, 2);
+
+    cout << treeCount << " trees taking " << treeArea << "units of space";
+
     return 0;
 }
