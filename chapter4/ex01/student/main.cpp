9046c1d57704809744991c1a626ecb3a09079847
CoolPixels <coolp_xels@proton.me>
2025-02-07 21:09:15 +0000

    Testing Fri Feb 07 2025 21:09:15 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex01/student/main.cpp
+++ chapter4/ex01/student/main.cpp
@@ -3,6 +3,20 @@
 using namespace std;
 
 int main() {
-    // Write your main here
+    int number;
+
+    cout << "Number: ";
+    cin >> number;
+
+    if (number == 0) {
+        cout << "Zero";
+    } else if (number < 0) {
+        cout << "Negative";
+    } else {
+        cout << "Positive";
+    }
+
+    cout << endl;
+
     return 0;
 }
