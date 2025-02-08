952e7fd03758dd6801bf08fe5e1a1fa7d4127bbc
CoolPixels <coolp_xels@proton.me>
2025-02-08 23:36:44 +0000

    Testing Sat Feb 08 2025 23:36:44 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex07/student/main.cpp
+++ chapter4/ex07/student/main.cpp
@@ -21,8 +21,8 @@ int main()
     else if (discriminant > 0)
     {
         cout << "real ";
-        const double root1 = (-b + sqrt(discriminant)) / 2 * a;
-        const double root2 = (-b - sqrt(discriminant)) / 2 * a;
+        const double root1 = (-b + sqrt(discriminant)) / 2.0 * a;
+        const double root2 = (-b - sqrt(discriminant)) / 2.0 * a;
         cout << root1 << ", " << root2;
     } else {
         cout << "complex";
