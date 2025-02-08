e162f038f23af360748ddff94c0c1ab6aee5201f
CoolPixels <coolp_xels@proton.me>
2025-02-08 21:43:04 +0000

    Testing Sat Feb 08 2025 21:43:04 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex02/student/main.cpp
+++ chapter4/ex02/student/main.cpp
@@ -8,32 +8,32 @@ int main()
     int num1, num2, num3;
     int used = 0;
 
-    cin >> num3 >> num2 >> num1;
+    cin >> num1 >> num2 >> num3;
 
     // sort numbers
     if (num1 > num2 && num2 > num3)
     {
-        cout << num1 << " " << num2 << " " << num3;
+        cout << num3 << " " << num2 << " " << num1;
     }
     else if (num1 > num3 && num3 > num2)
     {
-        cout << num1 << " " << num3 << " " << num2;
+        cout << num2 << " " << num3 << " " << num1;
     }
     else if (num2 > num1 && num1 > num3)
     {
-        cout << num2 << " " << num1 << " " << num3;
+        cout << num3 << " " << num1 << " " << num2;
     }
     else if (num2 > num3 && num3 > num1)
     {
-        cout << num2 << " " << num3 << " " << num3;
+        cout << num3 << " " << num3 << " " << num2;
     }
     else if (num3 > num1 && num1 > num2)
     {
-        cout << num3 << " " << num1 << " " << num2;
+        cout << num2 << " " << num1 << " " << num3;
     }
     else if (num3 > num2 && num2 > num1)
     {
-        cout << num3 << " " << num2 << " " << num1;
+        cout << num1 << " " << num2 << " " << num3;
     }
     cout << endl;
 
