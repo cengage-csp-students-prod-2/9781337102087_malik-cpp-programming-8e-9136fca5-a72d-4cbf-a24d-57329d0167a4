bccb12a17ddc4e4df3a34143bd0f6cd189a11c44
CoolPixels <coolp_xels@proton.me>
2025-02-08 23:41:12 +0000

    Testing Sat Feb 08 2025 23:41:12 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex07/student/main.cpp
+++ chapter4/ex07/student/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <cmath>
 
 using namespace std;
@@ -23,7 +24,7 @@ int main()
         cout << "real ";
         const double root1 = (-b + sqrt(discriminant)) / (2.0 * a);
         const double root2 = (-b - sqrt(discriminant)) / (2.0 * a);
-        cout << root1 << ", " << root2;
+        cout << fixed << setprecision(2) << root1 << ", " << root2;
     } else {
         cout << "complex";
     }
