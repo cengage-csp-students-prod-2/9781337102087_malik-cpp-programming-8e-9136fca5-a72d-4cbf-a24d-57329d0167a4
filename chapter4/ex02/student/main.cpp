3523ba76fcc9a8b537924202224ae1f6a212ae4e
CoolPixels <coolp_xels@proton.me>
2025-02-08 21:37:56 +0000

    Testing Sat Feb 08 2025 21:37:56 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex02/student/main.cpp
+++ chapter4/ex02/student/main.cpp
@@ -2,7 +2,41 @@
 
 using namespace std;
 
-int main() {
-    // Write your main here
+int main()
+{
+    // get numbers
+    double inNum1, inNum2, inNum3;
+    int used = 0;
+
+    cin >> inNum1 >> inNum2 >> inNum3;
+
+    // sort numbers
+    double num1, num2, num3;
+
+    if (num1 > num2 && num2 > num3)
+    {
+        cout << num1 << " " << num2 << " " << num3;
+    }
+    else if (num1 > num3 && num3 > num2)
+    {
+        cout << num1 << " " << num3 << " " << num2;
+    }
+    else if (num2 > num1 && num1 > num3)
+    {
+        cout << num2 << " " << num1 << " " << num3;
+    }
+    else if (num2 > num3 && num3 > num1)
+    {
+        cout << num2 << " " << num3 << " " << num3;
+    }
+    else if (num3 > num1 && num1 > num2)
+    {
+        cout << num3 << " " << num1 << " " << num2;
+    }
+    else if (num3 > num2 && num2 > num1)
+    {
+        cout << num3 << " " << num2 << " " << num1;
+    }
+
     return 0;
 }
