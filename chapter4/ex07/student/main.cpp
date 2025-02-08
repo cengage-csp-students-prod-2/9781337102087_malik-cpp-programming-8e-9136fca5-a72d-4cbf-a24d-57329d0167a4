39638c81308ddbe137ae16ce4e711760ed32b43c
CoolPixels <coolp_xels@proton.me>
2025-02-08 23:28:05 +0000

    Testing Sat Feb 08 2025 23:28:05 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex07/student/main.cpp
+++ chapter4/ex07/student/main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-int main() {
-    // Write your main here
-    return 0;
+int main()
+{
+    // get terms
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    // run quadratic formula
+    const double discriminant = pow(b, 2) - 4 * a * c;
+
+    if (discriminant == 0)
+    {
+        const double root = -b / 2 * a;
+        cout << root;
+    }
+    else if (discriminant > 0)
+    {
+        const double root1 = (-b + pow(discriminant, -1)) / 2 * a;
+        const double root2 = (-b - pow(discriminant, -1)) / 2 * a;
+        cout << root1 << ", " << root2;
+    }
 }
