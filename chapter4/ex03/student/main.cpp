e03313524712012d97cca3908f59352c3213c623
CoolPixels <coolp_xels@proton.me>
2025-02-08 22:35:26 +0000

    Testing Sat Feb 08 2025 22:35:26 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex03/student/main.cpp
+++ chapter4/ex03/student/main.cpp
@@ -3,6 +3,16 @@
 using namespace std;
 
 int main() {
-    // Write your main here
+    // get input
+    int input;
+    cin >> input;
+
+    if (input < 10) {
+        cout << input;
+    } else {
+        cout << static_cast<char>(input + 55);
+    }
+    cout << endl;
+
     return 0;
 }
