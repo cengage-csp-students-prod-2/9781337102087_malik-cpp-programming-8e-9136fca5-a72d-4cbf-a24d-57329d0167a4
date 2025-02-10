1006a7bdeb28d7b12bcea9a5f0e2ad064b3ac20b
CoolPixels <coolp_xels@proton.me>
2025-02-10 03:59:04 +0000

    Testing Mon Feb 10 2025 03:59:04 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex20/student/main.cpp
+++ chapter4/ex20/student/main.cpp
@@ -1,8 +1,64 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
-int main() {
-    // Write your main here
-    return 0;
+int main()
+{
+    // constants
+    const int discount1 = 10;
+    const int discount2 = 20;
+    const int discount3 = 30;
+    const int discount4 = 5;
+
+    double roomCost;
+    int roomsBooked;
+    int bookingDuration;
+    double salesTax;
+
+    cout << "Cost of one room: $";
+    cin >> roomCost;
+    cout << "Rooms booked: ";
+    cin >> roomsBooked;
+    cout << "Booking duration: ";
+    cin >> bookingDuration;
+    cout << "Sales tax: ";
+    cin >> salesTax;
+
+    int totalCost = roomCost * roomsBooked;
+    double priceRatio = 1;
+
+    if (roomsBooked >= 30)
+    {
+        roomCost -= roomCost * (discount3 / 100);
+        priceRatio -= priceRatio * (discount3 / 100);
+    }
+    else if (roomsBooked >= 20)
+    {
+        roomCost -= roomCost * (discount2 / 100);
+        priceRatio -= priceRatio * (discount2 / 100);
+    }
+    else if (roomsBooked >= 10)
+    {
+        roomCost -= roomCost * (discount1 / 100);
+        priceRatio -= priceRatio * (discount1 / 100);
+    }
+
+    if (bookingDuration >= 3)
+    {
+        roomCost -= roomCost * (discount4 / 100);
+        priceRatio -= priceRatio * (discount4 / 100);
+    }
+
+    cout << fixed << setprecision(2);
+
+    double salesTax = roomCost * roomsBooked * (salesTax / 100);
+
+    cout << "$" << roomCost + salesTax / roomsBooked << endl;
+    cout << (1 - priceRatio) * 100 << "%" << endl;
+    cout << roomsBooked << endl;
+    cout << bookingDuration << endl;
+    cout << "$" << roomCost * roomsBooked;
+    cout << "$" << salesTax;
+    cout << "$" << roomCost * roomsBooked + salesTax;
 }
