966b82bb2f2f405b47b36b092e3cfe2cc3cf5ac3
CoolPixels <coolp_xels@proton.me>
2025-02-10 01:17:54 +0000

    Testing Mon Feb 10 2025 01:17:54 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex09/student/main.cpp
+++ chapter4/ex09/student/main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main() {
-    // Write your main here
-    return 0;
+    // get input
+    float num1, num2;
+    char op;
+
+    cin >> num1 >> op >> num2;
+    cout << endl << fixed << setprecision(2);
+
+    if (op == '+') {
+        cout << num1 + num2;
+    } else if (op == '-') {
+        cout << num1 - num2;
+    } else if (op == '*') {
+        cout << num1 * num2;
+    } else {
+        if (num2 == 0.0) {
+            cout << "error";
+            return 0;
+        }
+
+        cout << num1 / num2;
+    }
 }
