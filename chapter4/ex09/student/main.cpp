a6f212db3cf2dd6c82ab37ee23cfb2dcc6331f85
CoolPixels <coolp_xels@proton.me>
2025-02-10 02:41:06 +0000

    Testing Mon Feb 10 2025 02:41:06 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex09/student/main.cpp
+++ chapter4/ex09/student/main.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
-int main() {
-    // Write your main here
+int main()
+{
+    // get input
+    double num1, num2;
+    char op;
+
+    cin >> num1 >> op >> num2;
+
+    if (op == '+')
+    {
+        cout << num1 << " + " << num2 << " = " << fixed << setprecision(2)
+             << num1 + num2;
+    }
+    else if (op == '-')
+    {
+        cout << num1 << " - " << num2 << " = " << fixed << setprecision(2)
+             << num1 - num2;
+    }
+    else if (op == '*')
+    {
+        cout << num1 << " * " << num2 << " = " << fixed << setprecision(2)
+             << num1 * num2;
+    }
+    else if (op == '/')
+    {
+        if (num2 == 0.0)
+        {
+            cout << "error" << endl;
+            return 0;
+        }
+
+        cout << num1 << " / " << num2 << " = " << fixed << setprecision(2)
+             << num1 / num2;
+    }
+    else
+    {
+        cout << "Invalid operator";
+    }
+    cout << endl;
+
     return 0;
 }
