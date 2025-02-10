6d9a406f2939e03001974b5ec7db5d0840cebbac
CoolPixels <coolp_xels@proton.me>
2025-02-10 02:40:58 +0000

    Reset Exercise Mon Feb 10 2025 02:40:58 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex09/student/main.cpp
+++ chapter4/ex09/student/main.cpp
@@ -1,44 +1,8 @@
 #include <iostream>
-#include <iomanip>
 
 using namespace std;
 
-int main()
-{
-    // get input
-    double num1, num2;
-    char op;
-
-    cin >> num1 >> op >> num2;
-    cout << fixed << setprecision(2);
-
-    if (op == '+')
-    {
-        cout << num1 << " + " << num2 << " = " << num1 + num2;
-    }
-    else if (op == '-')
-    {
-        cout << num1 << " - " << num2 << " = " << num1 - num2;
-    }
-    else if (op == '*')
-    {
-        cout << num1 << " * " << num2 << " = " << num1 * num2;
-    }
-    else if (op == '/')
-    {
-        if (num2 == 0.0)
-        {
-            cout << "error" << endl;
-            return 0;
-        }
-
-        cout << num1 << " / " << num2 << " = " << num1 / num2;
-    }
-    else
-    {
-        cout << "Invalid operator";
-    }
-    cout << endl;
-
+int main() {
+    // Write your main here
     return 0;
 }
