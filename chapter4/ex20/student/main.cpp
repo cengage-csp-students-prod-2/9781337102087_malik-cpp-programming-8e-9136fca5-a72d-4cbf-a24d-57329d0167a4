b6df914e87c0ac1a913ac63b5ee3dc9ed145eb5f
CoolPixels <coolp_xels@proton.me>
2025-02-10 04:00:28 +0000

    Testing Mon Feb 10 2025 04:00:28 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex20/student/main.cpp
+++ chapter4/ex20/student/main.cpp
@@ -5,60 +5,62 @@ using namespace std;
 
 int main()
 {
-    // constants
-    const int discount1 = 10;
-    const int discount2 = 20;
-    const int discount3 = 30;
-    const int discount4 = 5;
+    // Constants for discounts
+    const double DISCOUNT_10_ROOMS = 10.0;
+    const double DISCOUNT_20_ROOMS = 20.0;
+    const double DISCOUNT_30_ROOMS = 30.0;
+    const double DISCOUNT_3_DAYS = 5.0;
 
+    // Input variables
     double roomCost;
     int roomsBooked;
     int bookingDuration;
-    double salesTax;
+    double taxRate;
 
+    // Taking inputs
     cout << "Cost of one room: $";
     cin >> roomCost;
     cout << "Rooms booked: ";
     cin >> roomsBooked;
-    cout << "Booking duration: ";
+    cout << "Booking duration (days): ";
     cin >> bookingDuration;
-    cout << "Sales tax: ";
-    cin >> salesTax;
+    cout << "Sales tax (%): ";
+    cin >> taxRate;
 
-    int totalCost = roomCost * roomsBooked;
-    double priceRatio = 1;
+    // Determine discount percentage
+    double discountPercent = 0.0;
 
     if (roomsBooked >= 30)
-    {
-        roomCost -= roomCost * (discount3 / 100);
-        priceRatio -= priceRatio * (discount3 / 100);
-    }
+        discountPercent = DISCOUNT_30_ROOMS;
     else if (roomsBooked >= 20)
-    {
-        roomCost -= roomCost * (discount2 / 100);
-        priceRatio -= priceRatio * (discount2 / 100);
-    }
+        discountPercent = DISCOUNT_20_ROOMS;
     else if (roomsBooked >= 10)
-    {
-        roomCost -= roomCost * (discount1 / 100);
-        priceRatio -= priceRatio * (discount1 / 100);
-    }
+        discountPercent = DISCOUNT_10_ROOMS;
 
     if (bookingDuration >= 3)
-    {
-        roomCost -= roomCost * (discount4 / 100);
-        priceRatio -= priceRatio * (discount4 / 100);
-    }
+        discountPercent += DISCOUNT_3_DAYS;
 
+    // Apply discount to room cost
+    double discountedRoomCost = roomCost * (1 - discountPercent / 100.0);
+
+    // Compute costs
+    double totalRoomCost = discountedRoomCost * roomsBooked * bookingDuration;
+    double totalSalesTax = totalRoomCost * (taxRate / 100.0);
+    double totalBillingAmount = totalRoomCost + totalSalesTax;
+
+    // Set decimal precision
     cout << fixed << setprecision(2);
 
-    double salesTax = roomCost * roomsBooked * (salesTax / 100);
+    // Output results
+    cout << "\nSummary of Booking:\n";
+    cout << "----------------------------------\n";
+    cout << "Original Cost of One Room: $" << roomCost << endl;
+    cout << "Discount Applied: " << discountPercent << "%\n";
+    cout << "Number of Rooms Booked: " << roomsBooked << endl;
+    cout << "Booking Duration: " << bookingDuration << " days\n";
+    cout << "Total Cost Before Tax: $" << totalRoomCost << endl;
+    cout << "Sales Tax Amount: $" << totalSalesTax << endl;
+    cout << "Total Billing Amount: $" << totalBillingAmount << endl;
 
-    cout << "$" << roomCost + salesTax / roomsBooked << endl;
-    cout << (1 - priceRatio) * 100 << "%" << endl;
-    cout << roomsBooked << endl;
-    cout << bookingDuration << endl;
-    cout << "$" << roomCost * roomsBooked;
-    cout << "$" << salesTax;
-    cout << "$" << roomCost * roomsBooked + salesTax;
+    return 0;
 }
