4e40f412c6782714079f53a569e1fd792d4f2acf
CoolPixels <coolp_xels@proton.me>
2025-02-10 04:33:51 +0000

    Testing Mon Feb 10 2025 04:33:51 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex21/student/main.cpp
+++ chapter4/ex21/student/main.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
+#include <iomanip>
+#include <cmath>
 
 using namespace std;
 
 int main() {
-    // Write your main here
+    // get coords
+    double x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+
+    cout << fixed << setprecision(2);
+
+    if (x1 == x2) {
+        cout << "vertical";
+    }
+    if (y1 == y2) {
+        cout << "horizontal";
+    }
+
+    const double rise = y1 - y2;
+    const double run = x1 - x2;
+
+    const double slope = rise / run;
+
+    if (slope > 0) {
+        cout << "increasing";
+    } else if (slope < 0) {
+        cout << "decreasing";
+    }
+
+    cout << endl;
+
+    cout << slope;
+
     return 0;
 }
