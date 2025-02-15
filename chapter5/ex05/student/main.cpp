8b0eceb0a5eba23f763b5446552398bb374acbf0
CoolPixels <coolp_xels@proton.me>
2025-02-15 21:44:58 +0000

    Testing Sat Feb 15 2025 21:44:58 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex05/student/main.cpp
+++ chapter5/ex05/student/main.cpp
@@ -2,7 +2,31 @@
 
 using namespace std;
 
-int main() {
-    // Write your main here
+int main()
+{
+    // get input
+    int num1, num2;
+    cout << "Enter two integers: ";
+    cin >> num1 >> num2;
+
+    // sort input
+    int numMin, numMax;
+    if (num1 < num2)
+    {
+        numMin = num1;
+        numMax = num2;
+    }
+    else
+    {
+        numMin = num2;
+        numMax = num1;
+    }
+
+    // iterate through ints between input
+    for (int i = numMin + 1; i < numMax; i++)
+    {
+        cout << i << endl;
+    }
+
     return 0;
 }
