e91b2e605fa6decaf72686a63955dd24348c888b
CoolPixels <coolp_xels@proton.me>
2025-02-16 14:21:06 +0000

    Testing Sun Feb 16 2025 14:21:06 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex08/student/main.cpp
+++ chapter5/ex08/student/main.cpp
@@ -3,6 +3,19 @@
 using namespace std;
 
 int main() {
-    // Write your main here
-    return 0;
+    int firstNum, secondNum;
+    cout << "Enter two integers: ";
+    cin >> firstNum >> secondNum;
+
+    int i = firstNum;
+    int oddNumbers = 0;
+    while (i <= secondNum) {
+        if (i % 2 == 1) {
+            cout << i << " ";
+            oddNumbers += 1;
+        }
+        i++;
+    }
+
+    cout << endl << "Total odd numbers: " << oddNumbers << endl;
 }
