4df827a51c3bce050d5cc96d8e03c8431b018483
CoolPixels <coolp_xels@proton.me>
2025-02-16 14:29:49 +0000

    Testing Sun Feb 16 2025 14:29:49 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex08/student/main.cpp
+++ chapter5/ex08/student/main.cpp
@@ -1,24 +1,47 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-int main() {
+int main()
+{
+    // stuff with the numbers
     int firstNum, secondNum;
     cout << "Enter two integers: ";
     cin >> firstNum >> secondNum;
 
     int i = firstNum;
     int oddNumbers = 0;
-    int sum = 0;
-    while (i <= secondNum) {
-        if (i % 2 == 1) {
+    int evenSum = 0;
+    int oddSquaredSum = 0;
+    while (i <= secondNum)
+    {
+        if (i % 2 == 1)
+        {
             cout << i << " ";
             oddNumbers += 1;
-        } else {
-            sum += i;
+            oddSquaredSum += pow(i, 2);
+        }
+        else
+        {
+            evenSum += i;
         }
         i++;
     }
 
-    cout << endl << "Total odd numbers: " << oddNumbers << ", sum of even numbers " << sum << endl;
+    cout << endl
+         << "Total odd numbers: " << oddNumbers << ", sum of even numbers " << evenSum << ", sum of odd numbers squared " << oddSquaredSum << endl;
+
+    // squares
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << i << " " << pow(i, 2) << endl;
+    }
+
+    // alphabet
+    for (int i = 65; i <= 90; i++)
+    {
+        cout << (char)i << " ";
+    }
+    cout << endl;
 }
