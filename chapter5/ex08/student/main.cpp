c36221be957ed9a0f09ad09ecfe2820c04a3dbe9
CoolPixels <coolp_xels@proton.me>
2025-02-16 14:21:59 +0000

    Testing Sun Feb 16 2025 14:21:59 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex08/student/main.cpp
+++ chapter5/ex08/student/main.cpp
@@ -9,13 +9,15 @@ int main() {
 
     int i = firstNum;
     int oddNumbers = 0;
+    int sum = 0;
     while (i <= secondNum) {
         if (i % 2 == 1) {
             cout << i << " ";
             oddNumbers += 1;
+            sum += i;
         }
         i++;
     }
 
-    cout << endl << "Total odd numbers: " << oddNumbers << endl;
+    cout << endl << "Total odd numbers: " << oddNumbers << ", sum " << sum << endl;
 }
