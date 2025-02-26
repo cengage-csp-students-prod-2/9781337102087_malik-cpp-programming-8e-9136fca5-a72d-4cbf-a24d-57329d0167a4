d9e7325dc7e10ed520189caf7faaf547327c1d83
CoolPixels <coolp_xels@proton.me>
2025-02-26 11:50:20 +0000

    Testing Wed Feb 26 2025 11:50:19 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex03/student/main.cpp
+++ chapter6/ex03/student/main.cpp
@@ -2,7 +2,35 @@
 
 using namespace std;
 
-int main() {
-    // Write your main here
+bool isVowel(char ch);
+bool isVowel(char ch)
+{
+    const char lowercaseCharacter = tolower(ch);
+
+    if (lowercaseCharacter == 'a' || lowercaseCharacter == 'e' || lowercaseCharacter == 'i' || lowercaseCharacter == 'o' || lowercaseCharacter == 'u')
+    {
+        return true;
+    }
+
+    return false;
+}
+
+int main()
+{
+    cout << "Enter a sequence of characters: ";
+    string input;
+    cin >> input;
+
+    int vowels = 0;
+    for (int i = 0; i <= input.length(); i++)
+    {
+        if (isVowel(input[i]))
+        {
+            vowels += 1;
+        }
+    }
+
+    cout << "There are " << vowels << " vowels in this sentence." << endl;
+
     return 0;
 }
