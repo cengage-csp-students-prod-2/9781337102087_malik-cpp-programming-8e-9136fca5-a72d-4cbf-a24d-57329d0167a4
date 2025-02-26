cc4579b577b6d8f178b4c326fd99e25f586d60d2
CoolPixels <coolp_xels@proton.me>
2025-02-26 11:39:42 +0000

    Testing Wed Feb 26 2025 11:39:42 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex02/student/main.cpp
+++ chapter6/ex02/student/main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <cctype>
 
 using namespace std;
 
-int main() {
-    // Write your main here
+bool isVowel(char character)
+{
+    const char lowercaseCharacter = tolower(character);
+
+    if (lowercaseCharacter == 'a' || lowercaseCharacter == 'e' || lowercaseCharacter == 'i' || lowercaseCharacter == 'o' || lowercaseCharacter == 'u')
+    {
+        return true;
+    }
+
+    return false;
+}
+
+int main()
+{
+    cout << "Enter a character: ";
+    char inputCharacter;
+    cin >> inputCharacter;
+
+    cout << inputCharacter << " is a vowel: " << isVowel(inputCharacter);
+
     return 0;
 }
