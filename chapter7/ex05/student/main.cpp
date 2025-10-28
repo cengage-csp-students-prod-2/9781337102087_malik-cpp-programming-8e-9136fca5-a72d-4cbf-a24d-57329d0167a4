c759367dd955eef0b96d43ea786aacf1837c656e
CoolPixels <coolp_xels@proton.me>
2025-10-28 18:23:15 +0000

    Testing Tue Oct 28 2025 18:23:15 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex05/student/main.cpp
+++ chapter7/ex05/student/main.cpp
@@ -1,8 +1,48 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
+bool married = false;
+double income;
+double pension;
+int children = 0;
+
+void getData() {
+    cout << "married? ";
+    string marriedInput;
+    cin >> marriedInput;
+    if (marriedInput == "married") {
+        married = true;
+    }
+
+    cout << "income? ";
+    cin >> income;
+
+    cout << "pension? ";
+    cin >> pension;
+
+    if (married) {
+        cout << "how many kids? ";
+        cin >> children;
+    }
+}
+
+double taxAmount() {
+    income -= married ? 7000 : 4000;
+    income -= income * (pension/100);
+    income -= ((married ? 2 : 1) + children) * 1500;
+
+    if (income > 0 && income < 15001) {
+        return income * 0.15;
+    } else if (income >= 15001 && income < 40001) {
+        return 2250 + ((income - 15000) * 0.25);
+    } else if (income >= 40001) {
+        return 8460 + ((income - 40000) * 0.35);
+    }
+}
+
 int main() {
-    // Write your main here
-    return 0;
+    getData();
+    cout << endl << "that'll be $" << round(taxAmount() * 100) / 100 << endl;
 }
