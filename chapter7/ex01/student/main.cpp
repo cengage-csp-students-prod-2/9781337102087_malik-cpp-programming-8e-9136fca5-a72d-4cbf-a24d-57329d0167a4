d9faad5abd7122dfbc728c65367ca0e532d64445
CoolPixels <coolp_xels@proton.me>
2025-10-28 16:36:49 +0000

    Testing Tue Oct 28 2025 16:36:49 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex01/student/main.cpp
+++ chapter7/ex01/student/main.cpp
@@ -2,7 +2,38 @@
 
 using namespace std;
 
+enum triangleType {
+  scalene,
+  isosceles,
+  equilateral,
+  noTriangle
+};
+
+triangleType triangleShape(float a, float b, float c) {
+    if (a == b == c) {
+        return equilateral;
+    } else if (a == b || a == c || b == c) {
+        return isosceles;
+    } else if (a + b > c && a + c > b && b + c > a) {
+        return scalene;
+    }
+
+    return noTriangle;
+}
+
 int main() {
-    // Write your main here
-    return 0;
+  float a, b, c;
+  cin >> a >> b >> c;
+
+  triangleType type = triangleShape(a, b, c);
+
+  if (type == isosceles) {
+    cout << "isosceles";
+  } else if (type == equilateral) {
+    cout << "equilateral";
+  } else if (type == scalene) {
+    cout << "scalene";
+  } else {
+    cout << "no triangle";
+  }
 }
