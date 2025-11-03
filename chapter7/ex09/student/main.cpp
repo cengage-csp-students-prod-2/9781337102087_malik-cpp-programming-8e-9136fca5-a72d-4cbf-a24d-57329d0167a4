e7d02e283953bae7b472e8e3a7534f2c99d2bfc8
CoolPixels <coolp_xels@proton.me>
2025-11-03 05:28:19 +0000

    Testing Mon Nov 03 2025 05:28:19 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex09/student/main.cpp
+++ chapter7/ex09/student/main.cpp
@@ -3,14 +3,7 @@
 using namespace std;
 
 int main() {
-    /*
-    Miller, Jason Brian
-    Blair, Lisa Maria
-    Gupta, Anil Kumar
-    Arora, Sumit Sahil
-    Saleh, Rhonda Beth
-    Spilner, Brody
-    */
+    
     cout << "Jason Brian Miller\nLisa Maria Blair\nAnil Kumar Gupta\n";
     cout << "Sumit Sahil Arora\nRhonda Beth Saleh\nBrody Spilner";
     return 0;
