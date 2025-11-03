0fd841c6f54104e219e0bd2f8ac7530463f55d39
CoolPixels <coolp_xels@proton.me>
2025-11-03 05:41:29 +0000

    Testing Mon Nov 03 2025 05:41:29 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex11/student/main.cpp
+++ chapter7/ex11/student/main.cpp
@@ -7,7 +7,7 @@ int main() {
     cin >> input;
 
     int sum = 0;
-    for (int i = 0; i <= input.length(); i++) {
+    for (int i = 0; i < input.length(); i++) {
         cout << static_cast<int>(input[i]) << '\n';
         sum += static_cast<int>(input[i]);
     }
