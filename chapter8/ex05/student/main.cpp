854dc2ac78e9f6db783e5192f2bc3ae0ee9d6efc
CoolPixels <coolp_xels@proton.me>
2025-11-05 23:45:25 +0000

    Testing Wed Nov 05 2025 23:45:25 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex05/student/main.cpp
+++ chapter8/ex05/student/main.cpp
@@ -3,6 +3,20 @@
 using namespace std;
 
 int main() {
-    // Write your main here
+    char string[50];
+    cin >> string;
+
+    for (int i = 0; i < 50; i++) {
+        char c = string[i];
+        if (c == '\0') {
+            break;
+        }
+        if (static_cast<int>(c) <= 90) {
+            cout << c;
+        } else {
+            cout << static_cast<char>(c - 32);
+        }
+    }
+
     return 0;
 }
