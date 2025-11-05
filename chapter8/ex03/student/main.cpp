fe64eb3d87c302aa43e638e5f9b092d6bbb4b3f9
CoolPixels <coolp_xels@proton.me>
2025-11-05 23:04:34 +0000

    Testing Wed Nov 05 2025 23:04:34 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex03/student/main.cpp
+++ chapter8/ex03/student/main.cpp
@@ -3,6 +3,20 @@
 using namespace std;
 
 int main() {
-    // Write your main here
+    int arr[15];
+
+    for (int i = 0; i < 15; i++) {
+        cin >> arr[i];
+    }
+
+    int largestIndex = 0;
+    for (int i = 1; i < 15; i++) {
+        if (arr[i] > arr[largestIndex]) {
+            largestIndex = i;
+        }
+    }
+
+    cout << "(" << largestIndex << ", " << arr[largestIndex] << ")";
+
     return 0;
 }
