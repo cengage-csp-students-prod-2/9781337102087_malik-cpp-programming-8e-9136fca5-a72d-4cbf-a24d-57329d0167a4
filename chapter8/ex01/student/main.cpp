718a3bc3d2ff824141e8e04800a9e8d828e1032f
CoolPixels <coolp_xels@proton.me>
2025-11-05 22:51:55 +0000

    Testing Wed Nov 05 2025 22:51:55 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex01/student/main.cpp
+++ chapter8/ex01/student/main.cpp
@@ -3,6 +3,24 @@
 using namespace std;
 
 int main() {
-    // Write your main here
+    int length = 50;
+
+    int arr[length];
+
+    for (int i = 0; i < length; ++i) {
+        if (i < 25) {
+            arr[i] = i * i;
+        } else {
+            arr[i] = i * 3;
+        }
+    }
+
+    for (int i = 0; i < length; ++i) {
+        cout << arr[i] << " ";
+        if ((i + 1) % 10 == 0) {
+            cout << endl;
+        }
+    }
+
     return 0;
 }
