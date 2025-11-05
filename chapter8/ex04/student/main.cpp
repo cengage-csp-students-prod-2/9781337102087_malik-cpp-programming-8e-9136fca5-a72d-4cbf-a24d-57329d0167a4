cabf4547d1a4d07e75bb6fac531f101d3eb84a82
CoolPixels <coolp_xels@proton.me>
2025-11-05 23:19:32 +0000

    Testing Wed Nov 05 2025 23:19:32 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex04/student/main.cpp
+++ chapter8/ex04/student/main.cpp
@@ -3,6 +3,26 @@
 using namespace std;
 
 int main() {
-    // Write your main here
-    return 0;
+  int scores[] = {76,  89,  150, 135, 200, 76,  12,  100, 150,
+                  28,  178, 189, 167, 200, 175, 150, 87,  99,
+                  129, 149, 176, 200, 87,  35,  157, 189};
+  // shouldn't have given me input data with commas :p
+  // c++ file i/o makes me need to pee
+
+  int brackets[8];
+
+  for (int i = 0; i < 8; i++) {
+    brackets[i] = 0;
+  }
+
+  for (int score : scores) {
+    brackets[score / 25]++;
+  }
+
+  for (int i = 0; i < 8; i++) {
+    cout << i * 25 << "-" << (i + 1) * 25 - 1 << ": " << brackets[i] << endl;
+  }
+
+  // Write your main here
+  return 0;
 }
