c6ce9adfdc8b6650004b9837b9e40ab900225662
CoolPixels <coolp_xels@proton.me>
2025-11-26 04:51:49 +0000

    Testing Wed Nov 26 2025 04:51:49 GMT+0000 (Coordinated Universal Time)

--- chapter10/ex09/student/clockTypeImp.cpp
+++ chapter10/ex09/student/clockTypeImp.cpp
@@ -29,9 +29,26 @@ void clockType::printTime() const {
   std::cout << sec << std::endl;
 }
 
-void clockType::incrementSeconds() { sec++; };
-void clockType::incrementMinutes() { min++; };
-void clockType::incrementHours() { hr++; };
+void clockType::incrementSeconds() {
+  sec++;
+  if (sec >= 60) {
+    sec = 0;
+    incrementMinutes();
+  }
+};
+void clockType::incrementMinutes() {
+  min++;
+  if (min >= 60) {
+    min = 0;
+    incrementHours();
+  }
+};
+void clockType::incrementHours() {
+  hr++;
+  if (hr >= 23) {
+    hr = 0;
+  }
+};
 
 bool clockType::equalTime(const clockType& clock) const {
   int hour, minute, second;
--- chapter10/ex09/student/main.cpp
+++ chapter10/ex09/student/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include "clockType.h"
 
 using namespace std;
 
 int main() {
-    // Write your main here
+    auto clock = clockType();
+    clock.setTime(23, 59, 59);
+    clock.printTime();
+    clock.incrementSeconds();
+    clock.printTime();
+
+    auto otherClock = clockType();
+    otherClock.setTime(0, 0, 0);
+    cout << clock.equalTime(otherClock);
+    
     return 0;
 }
